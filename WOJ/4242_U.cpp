037d40d8e265cb55a93b42eb091d214806030756
Catty2014 <18111431031@163.com>
2019-07-31 20:09:42 +0800

    20190731,Night

--- WOJ/4242_U.cpp
+++ WOJ/4242_U.cpp
@@ -17,9 +17,11 @@ int e_cnt;
 int n,m;
 int d[N];
 int u[N];
-bool rn[N];
+int rn[N];
+int rnc=0;
 int ch[N],dg[N];
-bool vi1[N];
+int vi1[N];
+int vi2[N];
 
 void add(int u,int v,int w)
 {
@@ -31,48 +33,59 @@ void add(int u,int v,int w)
 }
 
 
-void loop_dfs(int u);
-
-void loop(int u)//Actually DFS
+int from1,to1;
+int q[N];int p;
+void loop(int u,int fa)//Actually DFS
 {
-    for(int i=fir[u];i++;i=e[i].nx)
+    if(u==1&&fa!=0) 
     {
-        int v=e[i].v;
-        if(vi1[v]==1)
+        if (from1==to1) p=0;
+        else 
         {
-            rn[u]=1;
-            return ;
+            rnc=p;
+            for(int i=1;i<=p;i++)
+            {
+                rn[i]=q[i];
+            }
         }
-        vi1[v]=true;
-        loop_dfs(v);
     }
-}
-
-void loop_dfs(int u)
-{
     for(int i=fir[u];i;i=e[i].nx)
     {
+        if(p!=0) return ;
         int v=e[i].v;
-        if(vi1[v]==1) continue;
-        vi1[v]=1;
-        loop_dfs(v);
+        if(v==fa) continue;
+        if(v==1)
+        {
+            to1=i;
+        }
+        if(u==1)
+        {
+            from1=i;
+        }
+        q[++p]=v;
+        loop(v,u);
+        if(p!=0) return ;
+        p--;
     }
 }
-
 
 int down(int x)
 {
-    if(d[x]!=0) return d[x];
+    if(d[x]!=-1) return d[x];
 }
 
 int up(int x)
 {
-    if(u[x]!=0) return u[x];
+    if(u[x]!=-1) return u[x];
 }
 
 
 int main(void)
 {
+    #ifdef FILEOUT
+        freopen("tmp.in","r",stdin);
+        freopen("tmp.out","w",stdout);
+    #endif
     scanf("%d %d",&n,&m);
     for(int i=1;i<=m;i++)
     {
@@ -81,16 +94,18 @@ int main(void)
         add(u,v,w);
         add(v,u,w);
     }
-    for(int i=1;i<=n;i++)
+    vi1[1]=1;
+    loop(1,0);
+    if (rnc==0)
     {
-        loop(i);
-    }
-    db ans=0.0;
-    for(int i=1;i<=n;i++)
-    {
-        ans+=(down(i)*ch[i]+up(i)*(dg[i]-ch[i]))/dg[i];
-    }
-    ans/=n;
-    printf("%.2lf\n",ans);
+        
+    } 
+    /* db ans=0.0; */
+    /* for(int i=1;i<=n;i++) */
+    /* { */
+    /*     ans+=(down(i)*ch[i]+up(i)*(dg[i]-ch[i]))/dg[i]; */
+    /* } */
+    /* ans/=n; */
+    /* printf("%.2lf\n",ans); */
     return 0;
 }
